90a7a47199ec86a61fc3f534fe5f6b76875e269c
liangkhwai <liangkhwai@gmail.com>
2023-09-21 23:52:17 +0700

    response userId to frontend

---
+++ webhook.js
@@ -0,0 +1,83 @@
+const express = require("express");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const cors = require("cors");
+const app = express();
+const port = 1234;
+const qs = require("qs");
+app.use(bodyParser.json());
+
+const LINE_CHANNEL_LOGIN_ID = process.env.LINE_CHANNEL_LOGIN_ID;
+const LINE_CHANNEL_LOGIN_SECRET = process.env.LINE_CHANNEL_LOGIN_SECRET;
+const LINE_REDIRECT_URI =
+  "https://875c-2403-6200-8837-846d-9542-9f0f-6443-fe19.ngrok-free.app/callback";
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.get("/auth", (req, res) => {
+  const state = "BIS_SUSU"; // Optional: Add a state value for security
+  const authUrl =
+    `https://access.line.me/oauth2/v2.1/authorize?` +
+    `response_type=code` +
+    `&client_id=${LINE_CHANNEL_LOGIN_ID}` +
+    `&redirect_uri=${LINE_REDIRECT_URI}` +
+    `&state=${state}` +
+    `&scope=openid%20profile`;
+  res.redirect(authUrl);
+});
+
+app.get("/callback", async (req, res) => {
+  const { code } = req.query;
+  if (!code) {
+    res.status(400).send("Authentication failed");
+    return;
+  }
+
+  console.log(req.query);
+  console.log(code);
+  // Exchange the authorization code for an access token
+  try {
+    const tokenResponse = await axios.post(
+      "https://api.line.me/oauth2/v2.1/token",
+      qs.stringify({
+        grant_type: "authorization_code",
+        code: code,
+        redirect_uri: LINE_REDIRECT_URI,
+        client_id: LINE_CHANNEL_LOGIN_ID,
+        client_secret: LINE_CHANNEL_LOGIN_SECRET,
+      })
+    );
+
+    const { access_token } = tokenResponse.data;
+    // Use the access token to fetch the user's profile, including userId
+    const profileResponse = await axios.get("https://api.line.me/v2/profile", {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+    // console.log(profileResponse);
+    const { userId } = profileResponse.data;
+    console.log("userId", userId);
+    // res.send(`Authentication successful. Your LINE userId: ${userId}`);
+    // res.redirect(
+    //   "https://line.me/R/ti/p/@124mwdzz?from=page&accountId=124mwdzz"
+    // );
+    res.redirect(
+      "http://localhost:3000/dashboard/line/invite?userId=" + userId
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error occurred during authentication");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
